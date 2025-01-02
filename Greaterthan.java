6fa734734e8a15af0b899a222355b128b457bf68
tanishtambe <tanishtambe13@gmail.com>
2025-01-03 01:02:02 +0530

    Task1

--- Greaterthan.java
+++ Greaterthan.java
@@ -10,7 +10,7 @@ public class Greaterthan {
 		int num2=Sc.nextInt();
 		
 		if(num1<num2) {
-			System.out.println(num1+ " less than" +num2);
+			System.out.println(num1+ " less than number2" +num2 );
 		}
 		else if (num1>num2) {
 			System.out.println(num1+" is greater than "+num2);
